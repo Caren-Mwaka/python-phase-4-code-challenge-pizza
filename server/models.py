9e8bd985eec2c2651fb5a249fb1d3bb4c2c7462c
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-07-07 11:57:42 +0300

    added getter and setter properties

--- server/models.py
+++ server/models.py
@@ -57,12 +57,22 @@ class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = "restaurant_pizzas"
 
     id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Integer, nullable=False)
+    _price = db.Column("price", db.Integer, nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
 
     restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
     pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be between 1 and 30")
+        self._price = value
 
     def to_dict(self):
         return {
@@ -73,12 +83,6 @@ class RestaurantPizza(db.Model, SerializerMixin):
             'restaurant': self.restaurant.to_dict(),
             'pizza': self.pizza.to_dict(),
         }
-
-    @validates('price')
-    def validate_price(self, key, price):
-        if price < 1 or price > 30:
-            raise ValueError("Price must be between 1 and 30")
-        return price
     
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
